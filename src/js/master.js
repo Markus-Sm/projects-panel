e9dbd8b1968e7df96342f415eeeb6a39e9bcda5d
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-01-02 08:51:17 +0100

    update

--- src/js/master.js
+++ src/js/master.js
@@ -1,10 +1,15 @@
 import "../style/master.scss";
+import "../style/components/_text-important.scss";
 import "jquery-ui-dist/jquery-ui.css";
 import "jquery-ui";
 
-$(document).ready(function () {
-  console.log("jQuery and jQuery UI are working!");
+// $(document).ready(function () {
+//   console.log("jQuery and jQuery UI are working!");
 
-  // Przykład użycia jQuery UI - dodajmy efekt draggable do nagłówka
+//   // Przykład użycia jQuery UI - dodajmy efekt draggable do nagłówka
+//   $(".plain-header__box").draggable();
+// });
+
+$(document).ready(function () {
   $(".plain-header__box").draggable();
 });
