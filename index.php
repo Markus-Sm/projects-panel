b67d09d12fc17a0ed5514b826e67ca990a78977e
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-02-04 09:22:10 +0100

    implementing instagram feed

--- index.php
+++ index.php
@@ -8,19 +8,7 @@ include "inc/header.inc.php";
       <div id="user-container"></div>
 
       <div class="grid-gallery">
-        <ul id="instagram-feed" class="grid-gallery__list">
-          <li>
-            <a
-              class="grid-gallery__item"
-              style="
-                background-image: url('https://cdn.pixabay.com/photo/2024/12/25/03/57/newborn-photography-9289496_1280.jpg');
-              "
-              href="https://cdn.pixabay.com/photo/2024/12/25/03/57/newborn-photography-9289496_1280.jpg"
-              title="Newborn photography by @marek.marek"
-              target="_blank"
-            ></a>
-          </li>
-        </ul>
+        <ul id="instagram-feed" class="grid-gallery__list"></ul>
       </div>
     </div>
     <script type="module" src="src/js/data.js"></script>
--- src/js/components/instagram-pureJs.js
+++ src/js/components/instagram-pureJs.js
@@ -1,7 +1,7 @@
 console.log("Instagram loaded");
 
 let domainUrl = "https://graph.instagram.com/me/media?";
-let fields = "fields=media_url";
+let fields = "fields=media_url,media_type,thumbnail_url";
 var access_token =
   "IGAAN20vFaDNpBZAFBHeVpJVjk1NVhVZAUZABeTMzeUhqcUFvVGhYTThjV3ZAOS0tkZA0k2RDE4UkEtc2F6U3A0YjVDMDNoamVic1hnU0o3VzBBUmxfXzJTRlY3cWE4NmV4bWhQbmIzeUljb2ZAqTjRfVHhqd2RBZADBKcEsxNGc0ZAnZAMNAZDZD";
 
@@ -54,19 +54,23 @@ fetch(fullUrl)
   .then((dataArray) => {
     if (dataArray.data) {
       dataArray.data.forEach((element) => {
-        document.querySelector(".grid-gallery").insertAdjacentHTML(
-          "beforeend",
+        const mediaUrl = element.media_type === 'VIDEO' ? element.thumbnail_url : element.media_url;
+        if (mediaUrl) {
+          document.querySelector(".grid-gallery").insertAdjacentHTML(
+            "afterbegin",
+            `
+            <li>
+              <a class="grid-gallery__item"
+                 style="background-image: url('${mediaUrl}');"
+                 href="${mediaUrl}"
+                 title="${element.caption || ""}"
+                 target="_blank">
+                 ${element.media_type === 'VIDEO' ? '<span class="video-indicator">▶️</span>' : ''}
+              </a>
+            </li>
           `
-          <li>
-            <a class="grid-gallery__item"
-               style="background-image: url('${element.media_url}');"
-               href="${element.permalink}"
-               title="${element.caption || ""}"
-               target="_blank">
-            </a>
-          </li>
-        `
-        );
+          );
+        }
       });
     }
   })
