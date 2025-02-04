abc508f12acb6eee1fab811381c57cde2312ed0a
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-02-04 11:18:16 +0100

    improved code performance

--- src/js/components/instagram-pureJs.js
+++ src/js/components/instagram-pureJs.js
@@ -1,6 +1,6 @@
 let domainUrl = "https://graph.instagram.com/me/media?";
 let fields = "fields=media_url,media_type,thumbnail_url,permalink,children{media_type,media_url}";
-let limit = "limit=20"; // Zwiększamy limit, aby znaleźć więcej zdjęć
+let limit = "limit=30"; // Zwiększamy limit, aby mieć większą szansę na znalezienie odpowiednich postów
 var access_token =
   "IGAAN20vFaDNpBZAFBHeVpJVjk1NVhVZAUZABeTMzeUhqcUFvVGhYTThjV3ZAOS0tkZA0k2RDE4UkEtc2F6U3A0YjVDMDNoamVic1hnU0o3VzBBUmxfXzJTRlY3cWE4NmV4bWhQbmIzeUljb2ZAqTjRfVHhqd2RBZADBKcEsxNGc0ZAnZAMNAZDZD";
 
@@ -52,31 +52,34 @@ fetch(fullUrl)
   })
   .then((dataArray) => {
     if (dataArray.data) {
-      const imagePosts = dataArray.data
-        .filter(post => {
-          const isImage = post.media_type === "IMAGE";
-          const isCarousel = post.media_type === "CAROUSEL_ALBUM";
-          const hasValidUrl = post.media_url;
-          return (isImage || isCarousel) && hasValidUrl;
-        })
-        .slice(0, 6);
-
-      document.querySelector(".grid-gallery").innerHTML = '';
+      const gridGallery = document.querySelector(".grid-gallery");
+      gridGallery.innerHTML = '';
       
-      imagePosts.forEach((element) => {
-        document.querySelector(".grid-gallery").insertAdjacentHTML(
-          "beforeend",
-          `
-          <li>
-            <a class="grid-gallery__item"
-               style="background-image: url('${element.media_url}');"
-               href="${element.permalink}"
-               title="${element.caption || ""}"
-               target="_blank">
-            </a>
-          </li>
-        `
-        );
+      let displayedImages = 0;
+      const maxImages = 6;
+      
+      // Iterujemy tylko raz przez dane
+      dataArray.data.some(post => {
+        if ((post.media_type === "IMAGE" || post.media_type === "CAROUSEL_ALBUM") && post.media_url) {
+          gridGallery.insertAdjacentHTML(
+            "beforeend",
+            `
+            <li>
+              <a class="grid-gallery__item"
+                 style="background-image: url('${post.media_url}');"
+                 href="${post.permalink}"
+                 title="${post.caption || ""}"
+                 target="_blank">
+              </a>
+            </li>
+            `
+          );
+          
+          displayedImages++;
+        }
+        
+        // Przerywamy iterację gdy mamy wystarczającą liczbę zdjęć
+        return displayedImages >= maxImages;
       });
     }
   })
