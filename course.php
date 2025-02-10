1f80f7f6e2e318873df5775ce810b2a6a391cd3a
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-02-10 22:14:43 +0100

    installing symfony

--- course.php
+++ course.php
@@ -4,12 +4,17 @@ $pageKey = "course";
 include "inc/header.inc.php";
 ?>
 
-<div>
+<div style="padding: 30px 40px;">
     <h1><?php echo $pageTitle; ?></h1>
 
     <?php
         $uri = $_SERVER['REQUEST_URI'];
-        $foo = $_GET['foo'] ?? null;
+        $foo = $_GET['id'] ?? null;
+
+        // write response 
+        echo "The URI request is: <pre>$uri</pre><br>\n";
+
+        echo "The value of the <pre>$pageKey</pre> parameter is: <pre>$foo</pre>";
     ?>
 
 </div>
