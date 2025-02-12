a8580fc9d77e571c263b2553b724ed775f985f7f
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-02-12 19:43:25 +0100

    update

--- course.php
+++ course.php
@@ -1,34 +1,13 @@
 <?php 
+
 $pageTitle = "Course Symfony";
 $pageKey = "course";
 include "inc/header.inc.php";
+
 ?>
 
 <div style="padding: 30px 40px;">
     <h1><?php echo $pageTitle; ?></h1>
-
-    <?php
-require_once __DIR__ . '/vendor/autoload.php'; 
-
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-
-        $request = Request::createFromGlobals();
-        $uri = $request->getPathInfo();
-        $foo = $request->get('foo', 'not set');
-
-        $uri = $_SERVER['REQUEST_URI'];
-        $foo = $_GET['id'] ?? null;
-
-        // prepate response 
-        $response = new Response();
-
-        $response->setStatusCode(Response::HTTP_OK);
-        $response->headers->set('Content-Type', 'text/html');
-        echo $response;
-
-        $response->send();
-    ?>
 
 </div>
 
--- index.php
+++ index.php
@@ -16,3 +16,26 @@ include "inc/header.inc.php";
 <?php 
 include "inc/footer.inc.php";
 ?>
+<?php
+require_once __DIR__ . '/vendor/autoload.php'; 
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+        $request = Request::createFromGlobals();
+        $uri = $request->getPathInfo();
+
+        if(in_array($uri, ['', '/'])){
+            $response = new Response("This is home page!");
+        }elseif($uri === '/about.php'){
+            $response = new Response("This is a contact page!");
+        }elseif($uri === '/course.php'){
+            $response = new Response("This is a course page!");
+        }elseif($uri === '/contact.php'){
+            $response = new Response("This is a contact page!");
+        }else {
+            $response = new Response('Not found', Response::HTTP_NOT_FOUND);
+        }
+
+        $response->send();
+    ?>
