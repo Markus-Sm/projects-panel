4cc24dca2b60c174978db55c4e37f46b78439dd2
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-04-16 16:47:55 +0200

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,1 +1,31 @@
+// import { UserCard } from "./components/UserCard.js";
 
+// console.log("Data loaded");
+
+// // Create instance of UserCard
+// const userCard = new UserCard("#user-container");
+
+// fetch("https://jsonplaceholder.typicode.com/users/1", {
+//   method: "GET",
+// })
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error("Something went wrong");
+//     }
+//   })
+//   .then((data) => {
+//     userCard.render(data);
+//   })
+//   .catch((error) => console.log(error));
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => {
+//     console.log(res.ok); // true
+//     console.log(res.status); // 200
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
